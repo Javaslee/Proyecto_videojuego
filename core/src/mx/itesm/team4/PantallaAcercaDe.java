3d633f5415b90a381c168defb49d3ffedc2da939
danzav <danielm.zavala@outlook.com>
2019-09-12 16:49:24 -0500

    Pantalla Acerca De - falta agregar texto variado

--- core/src/mx/itesm/team4/PantallaAcercaDe.java
+++ core/src/mx/itesm/team4/PantallaAcercaDe.java
@@ -15,7 +15,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-class PantallaMenu implements Screen {
+class PantallaAcercaDe implements Screen {
     private final Inicio inicio;
     //camara
     private OrthographicCamera camara;
@@ -28,10 +28,9 @@ class PantallaMenu implements Screen {
     private Texture texturaFondo;
 
     //escena de menu (botones)
-    private Stage escenaMenu;
+    private Stage escenaAD;
 
-
-    public PantallaMenu(Inicio inicio) {
+    public PantallaAcercaDe(Inicio inicio) {
         this.inicio=inicio;
     }
 
@@ -43,43 +42,26 @@ class PantallaMenu implements Screen {
     }
 
     private void crearMenu() {
-        escenaMenu=new Stage(vista);
+        escenaAD=new Stage(vista);
         //boton Inicio Juego
-        TextureRegionDrawable btnJugar=new TextureRegionDrawable(new TextureRegion(new Texture("button_juego.png")));
-        TextureRegionDrawable btnJugarOprimido= new TextureRegionDrawable(new TextureRegion(new Texture("button_juego_press.png")));
-        ImageButton btnInicioJuego= new ImageButton(btnJugar,btnJugarOprimido);
-        btnInicioJuego.setPosition(inicio.ANCHO/2-btnInicioJuego.getWidth()/2,2*inicio.ALTO/3);
+        TextureRegionDrawable btnRegresar=new TextureRegionDrawable(new TextureRegion(new Texture("button_regresar.png")));
+        TextureRegionDrawable btnRegresarOprimido= new TextureRegionDrawable(new TextureRegion(new Texture("button_regresar_pressed.png")));
+        ImageButton btnRegreso= new ImageButton(btnRegresar,btnRegresarOprimido);
+        btnRegreso.setPosition(0,inicio.ALTO-btnRegresar.getMinHeight());
         //Siguientes Botones
         //Evento boton
-        btnInicioJuego.addListener(new ClickListener(){
+        btnRegreso.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
                 //INSTRUCCIONES
-                inicio.setScreen(new PantallaNiveles(inicio));
-
+                inicio.setScreen(new PantallaMenu(inicio));
             }
         });
 
-        TextureRegionDrawable btnAcerDe=new TextureRegionDrawable(new TextureRegion(new Texture("button_acerca-de.png")));
-        TextureRegionDrawable btnAcerDeOprimido= new TextureRegionDrawable(new TextureRegion(new Texture("button_acerca-de_pressed.png")));
-        ImageButton btnInicioAcerca= new ImageButton(btnAcerDe,btnAcerDeOprimido);
-        btnInicioAcerca.setPosition(inicio.ANCHO/2-btnInicioAcerca.getWidth()/2,inicio.ALTO/2);
+        escenaAD.addActor(btnRegreso);
 
-        btnInicioAcerca.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                //INSTRUCCIONES
-                inicio.setScreen(new PantallaAcercaDe(inicio));
-
-            }
-        });
-
-        escenaMenu.addActor(btnInicioJuego);
-        escenaMenu.addActor(btnInicioAcerca);
-
-        Gdx.input.setInputProcessor(escenaMenu);
+        Gdx.input.setInputProcessor(escenaAD);
     }
 
     private void cargarTexturas() {
@@ -99,14 +81,14 @@ class PantallaMenu implements Screen {
     @Override
     public void render(float delta) {
         borrarPantalla();
-        //batch escala todo de acuerdo a la vista
+        //batch escala de acuerdo a la vista
         batch.setProjectionMatrix(camara.combined);
 
         batch.begin();
         batch.draw(texturaFondo,0,0);
         batch.end();
 
-        escenaMenu.draw();
+        escenaAD.draw();
     }
 
     private void borrarPantalla() {
