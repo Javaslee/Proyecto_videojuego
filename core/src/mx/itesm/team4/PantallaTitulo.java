1f1681e222ec04e06d695089684ffcd9c645a0f6
LivierAB <Livier.ab97@gmail.com>
2019-10-04 12:48:59 -0500

    Agregue una pantalla antes con solo el logo y al darle tap se cambia a pantalla menu

---
+++ core/src/mx/itesm/team4/PantallaTitulo.java
@@ -0,0 +1,160 @@
+package mx.itesm.team4;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class PantallaTitulo implements Screen {
+    private final Inicio inicio;
+
+    //camara
+    private OrthographicCamera camara;
+    private Viewport vista;
+
+    //batch
+    private SpriteBatch batch;
+
+    //fondo
+    private Texture texturaFondo;
+
+
+    //escena de menu (botones)
+    private Stage escenaTitulo;
+
+
+
+
+    PantallaTitulo(Inicio inicio){
+        this.inicio=inicio;
+    }
+
+    public void show(){
+        configurarVista();
+        cargarTexturas();
+        crearTitulo();
+
+        Gdx.input.setInputProcessor(new ProcesadorEntrada());
+
+
+    }
+
+    @Override
+    public void render(float delta) {
+        borrarPantalla();
+        //batch
+        batch.setProjectionMatrix(camara.combined);
+
+        batch.begin();
+        batch.draw(texturaFondo,0,0);
+        batch.end();
+
+        escenaTitulo.draw();
+
+    }
+
+    private void borrarPantalla() {
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        vista.update(width, height);
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    private void crearTitulo() {
+        escenaTitulo = new Stage(vista);
+
+    }
+
+    private void cargarTexturas() {
+        texturaFondo = new Texture("Titulo.png");
+    }
+
+    private void configurarVista() {
+        camara=new OrthographicCamera();
+        camara.position.set(inicio.ANCHO/2, inicio.ALTO/2, 0);
+        camara.update();
+
+        vista= new StretchViewport(inicio.ANCHO,inicio.ALTO, camara);
+
+        batch= new SpriteBatch();
+
+    }
+
+
+    private class ProcesadorEntrada implements InputProcessor {
+        @Override
+        public boolean keyDown(int keycode) {
+            return false;
+        }
+
+        @Override
+        public boolean keyUp(int keycode) {
+            return false;
+        }
+
+        @Override
+        public boolean keyTyped(char character) {
+            return false;
+        }
+
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            inicio.setScreen(new PantallaMenu(inicio));
+
+            return true;
+        }
+
+        @Override
+        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+            return false;
+        }
+
+        @Override
+        public boolean touchDragged(int screenX, int screenY, int pointer) {
+            return false;
+        }
+
+        @Override
+        public boolean mouseMoved(int screenX, int screenY) {
+            return false;
+        }
+
+        @Override
+        public boolean scrolled(int amount) {
+            return false;
+        }
+    }
+
+}
