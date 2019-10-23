0deb04203b1c45102575bf463ba294c5bf733f2e
LivierAB <Livier.ab97@gmail.com>
2019-10-23 11:19:31 -0500

    Ya sube y baja el personaje, pero vuela...

--- core/src/mx/itesm/team4/PantallaCamara.java
+++ core/src/mx/itesm/team4/PantallaCamara.java
@@ -222,6 +222,7 @@ class PantallaCamara extends Pantalla{
         @Override
         public boolean touchUp(int screenX, int screenY, int pointer, int button) {
             personaje.saltar(-50);
+
             return true;
         }
 
